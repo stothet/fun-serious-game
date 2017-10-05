48326ed4e778a95950c6c085a9b51f981a10e2e4
Jacky Lo <jlo755@aucklanduni.ac.nz>
2017-10-05 13:38:06 +1300

    Fixing missing files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Animator anim;
+	private bool playerMoving;
+	private Vector2 lastMove;
+	//private Rigidbody2D rb2d;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		playerMoving = false;
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) {
+			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
+		}
+
+		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) {
+			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime, 0f));
+			playerMoving = true;
+			lastMove = new Vector2 (0f,Input.GetAxisRaw ("Vertical"));
+		}
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat ("LastMoveY", lastMove.y);
+	}
+}
