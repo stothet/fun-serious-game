3bae72b4c503bc38399ca91779d7a291dfd58229
jxturtle <jxia616@aucklanduni.ac.nz>
2017-10-05 13:05:11 +1300

    Inventory so far
    05/10/17
    Bare-bone inventory. Add desired hard-coded desired items to the 'ItemDatabase' script, should not show up in the actual 'Slot' yet (to be implemented).

---
+++ Gumchew/Assets/Scripts/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+    public List<GameObject> _slots = new List<GameObject>();
+    public List<Item> _items = new List<Item>();
+    public GameObject slots;
+    int x = -110;
+    int y = 110;
+	// Use this for initialization
+	void Start () {
+	    for (int i = 1; i < 6; i++)
+        {
+            for (int j = 1; j < 6; j++)
+            {
+                GameObject slot = Instantiate(slots);
+                slot.transform.SetParent(this.gameObject.transform, false);
+                _slots.Add(slot);
+                //this.gameObject.transform.SetParent(slot.transform.parent);
+                //slot.transform.parent = this.gameObject.transform;
+                slot.name = "Inventory " + i + "." + j;
+                slot.GetComponent<RectTransform>().localPosition = new Vector3(x, y, 0);
+                x = x + 55;
+                if (j == 5)
+                {
+                    x = -110;
+                    y = y - 55;
+                }
+            }
+        }
+	}
+
+}
