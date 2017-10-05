291c8378fc9e12b08d8beabed7f7c0a8f504a35b
whattupjellycup <gman485@UoA.auckland.ac.nz>
2017-10-05 15:20:49 +1300

    starting minigame scene

---
+++ Assets/Scripts/Minigame Scripts/DragHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public static GameObject item; // i changed itembeigdraged to item. 
+    Transform startParent;
+    Vector3 startPosition;
+    bool start = true;
+    //Sprite sprite; 
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        item.GetComponent<LayoutElement>().ignoreLayout = true;
+        item.transform.SetParent(item.transform.parent.parent);
+    }
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData) {
+        item = null;
+        if (transform.parent == startParent) {
+            transform.position = startPosition;
+        }
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        item.GetComponent<LayoutElement>().ignoreLayout = false;
+    }
+}
