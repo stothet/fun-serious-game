09355b2c1e4d8de9d0ddd50cbe6857f9efd3df8b
sindhusingamneni <sindhusingamneni@gmail.com>
2017-10-06 18:27:36 +1300

    added main menu scene

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+
+    public EventSystem eventSystem;
+    public GameObject gameObject;
+
+    private bool selected;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && selected == false)
+        {
+            eventSystem.SetSelectedGameObject(gameObject);
+            selected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        selected = false;
+    }
+}
