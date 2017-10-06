6ab1464a91ba80b107652517886995869a0eda48
whattupjellycup <gman485@UoA.auckland.ac.nz>
2017-10-06 20:34:15 +1300

    fix drag and drop

---
+++ Assets/Scripts/Minigame Scripts/Drag.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject item; // item being dragged
+    Transform startParent;
+    Vector3 startPosition;
+    bool start = true;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        item = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        item = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.parent == startParent)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Minigame Scripts/Minigame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Minigame : MonoBehaviour {
+    [SerializeField]
+    private Text number1 = null;
+    private Text number2;
+    private Text number3;
+    private Text number4;
+
+
+    // Use this for initialization
+    void Start () {
+        //number1.text = "203";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
