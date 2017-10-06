012b51400d7f4eed8838690c54a287734c6c6a6a
jlo755 <jlo755@aucklanduni.ac.nz>
2017-10-06 17:33:52 +1300

    Implemented collision, and warping.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;
+	public Transform player;
 	private Vector3 offset;
+    Camera mycam;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;	
+		offset = transform.position - player.transform.position;
+        mycam = GetComponent<Camera>();
 	}
 
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+        mycam.orthographicSize = (Screen.height / 100f) / 4f;
+
+        if (player)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.position, 100f) + new Vector3(0, 0, - 10);
+        }
 	}
 }
