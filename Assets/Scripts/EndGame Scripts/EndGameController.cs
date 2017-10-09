ec2a296137e4abf83c488725835342e5b3510261
CJ <bchj513@hotmail.com>
2017-10-09 22:24:14 +1300

    Endgame scene implemented

--- Assets/Scripts/EndGame Scripts/EndGameController.cs
+++ Assets/Scripts/EndGame Scripts/EndGameController.cs
@@ -3,11 +3,11 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class EndGameManager : MonoBehaviour {
+public class EndGameController : MonoBehaviour {
     [SerializeField]
     public Text message = null;
     void Start()
     {
-	        message.text = TrialScript.endGameMessage;
+		message.text = TrialScript.endGameMessage;
 	}
 }
