888ae201e33583bdd3a62841596fbecb7d4d0e54
jxturtle <jxia616@aucklanduni.ac.nz>
2017-10-11 23:15:10 +1300

    Moved journal rework to Gyroscope branch
    Reworked journal to add slots instead of text entries

--- Assets/Scripts/Journal Scripts/Journal.cs
+++ Assets/Scripts/Journal Scripts/Journal.cs
@@ -6,15 +6,24 @@ using UnityEngine.UI;
 /// Handles the journal entries into the journal
 /// </summary>
 public class Journal : MonoBehaviour {
-    public Text journal;
-
+    public Text journalEntry;
+    public GameObject JournalSlot;
     public void Start()
     {
-        journal.text = PersistenceController.instance.journalState.journal;
+        //journalEntry.text = PersistenceController.JournalState.journal;
     }
+    
+    /// <summary>
+    /// Adds a new journal slot to the journal interface, populated with the desired text.
+    /// </summary>
+    /// <param name="txt">Text to be added to the journal slot entry.</param>
     public void putJournalEntry(string txt)
     {
-        PersistenceController.instance.journalState.journal += txt + "\n";
-        journal.text = PersistenceController.instance.journalState.journal;
+        GameObject slot = GameObject.Instantiate(JournalSlot);
+        journalEntry = slot.GetComponentInChildren<Text>();
+        slot.transform.SetParent(this.gameObject.transform, false);
+        journalEntry.text = txt;
+        //PersistenceController.JournalState.journal += txt;
+        //journalEntry.text = PersistenceController.JournalState.journal;
     }
 }
