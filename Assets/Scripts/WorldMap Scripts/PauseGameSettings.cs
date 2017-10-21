2c162d4926a91c70320da5934e8f7444e4fcffed
sindhusingamneni <sindhusingamneni@gmail.com>
2017-10-21 18:19:23 +1300

    Added in pause and resume

---
+++ Assets/Scripts/WorldMap Scripts/PauseGameSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGameSettings : MonoBehaviour {
+    public GameObject pauseButton;
+    public GameObject pausePanel;
+
+    public void Start()
+    {
+        unPauseGame();
+    }
+
+
+	public void pauseGame(){
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+    public void unPauseGame()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+
+
+}
