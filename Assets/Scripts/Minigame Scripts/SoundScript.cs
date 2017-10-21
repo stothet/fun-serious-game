06ebecd400d252571bcbbe0e9ec90cdb90533740
jxturtle <jxia616@aucklanduni.ac.nz>
2017-10-21 15:06:27 +1300

    Added script to detect sound
    Initial functionality. Prints something to the console when a loud enough sound is detected.

---
+++ Assets/Scripts/Minigame Scripts/SoundScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundScript : MonoBehaviour
+{
+    AudioClip microphoneInput;
+    bool microphoneInitialised;
+    public float sensitivity;
+
+    void Start()
+    {
+        //init microphone input
+        if (Microphone.devices.Length > 0)
+        {
+            microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
+            microphoneInitialised = true;
+        }
+    }
+
+    void Update()
+    {
+        //get mic volume
+        int dec = 128;
+        float[] waveData = new float[dec];
+        int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
+        microphoneInput.GetData(waveData, micPosition);
+
+        // Getting a peak on the last 128 samples
+        float levelMax = 0;
+        for (int i = 0; i < dec; i++)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
+
+        if (level>sensitivity)
+        {
+            Debug.Log("SOUND DETECTED");
+        }
+    }
+}
