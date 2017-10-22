267c9d6e2adb2f167d5e6259c2989984f6fcdf99
jlo755 <jlo755@aucklanduni.ac.nz>
2017-10-23 03:44:51 +1300

    Implemented Act 2 Scene.

---
+++ Assets/Scripts/BlowMinigameInitialiser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BlowMinigameInitialiser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("SCENE SWITCCCCH GUUURL");
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(5);
+        }
+    }
+}
