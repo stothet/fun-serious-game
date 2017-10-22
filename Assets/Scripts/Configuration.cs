4302d7f4be3a840b9953599b224c8ac8101a0c03
HeVictor <vhe821@aucklanduni.ac.nz>
2017-10-23 11:17:23 +1300

    Edited Jimmy dialogue and evidence descriptions.

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -42,9 +42,9 @@ public class Configuration
 	public static readonly string blueDrinkName = "Blue drink";
 
     // journal descriptions
-    public static readonly string bruceRegisterEntryDescription = "Register entry for Bruce, with his signature and an official timestamp.";
+    public static readonly string bruceRegisterEntryDescription = "Register entry for Bruce, with his signature for signing in and out. Timestamp states it's from 12.10 to 12.25.";
     public static readonly string billArtHistoryReportDescription = "Bill's Art History report, scrunched to a ball.";
-    public static readonly string detentionNoteDescription = "Detention note for Bruce, with the date and time nowhere to be seen.";
+    public static readonly string detentionNoteDescription = "A detention note for Bruce Buckley, the time period says 12.30 to 1. The handwriting seems a bit messy.";
     public static readonly string blueDrinkDescription = "What in tarnation is this?";
 
 }
