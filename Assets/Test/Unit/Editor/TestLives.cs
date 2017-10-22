becba63fb39868e5ae7e11c2f0222dc161a7bfc5
xavier630 <xavier630@gmail.com>
2017-10-22 16:42:15 +1300

    Remove integration test folder

---
+++ Assets/Test/Unit/Editor/TestLives.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class TestLives : MonoBehaviour {
+
+
+    /// <summary>
+    /// Sets up the static classes that are needed for the tests
+    /// </summary>
+    [SetUp]
+    public void establish()
+    {
+        PersistenceController.InstantiateInstance();
+    }
+
+    /// <summary>
+    /// Checks that the number of lives can be changed. Other methods in this class interact with the GUI which seems very difficult to mock.,
+    /// </summary>
+    [Test]
+    public void testExternalClassesCanUpdateState()
+    {
+    
+        UpdateLivesGUI gui = new UpdateLivesGUI();
+        PersistenceController.instance.playerState.lives = 3;
+        PersistenceController.instance.playerState.lives = 1;
+        Assert.Pass();
+
+    }
+}
