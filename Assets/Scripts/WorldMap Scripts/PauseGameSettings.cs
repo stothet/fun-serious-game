33b5196ff8a66482569d1aebb7943506c7ebf643
sindhusingamneni <sindhusingamneni@gmail.com>
2017-10-22 14:46:45 +1300

    Implemented the pause menu

---
+++ Assets/Scripts/WorldMap Scripts/PauseGameSettings.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseGameSettings : MonoBehaviour
+{
+    public GameObject pauseButton;
+    public GameObject pausePanel;
+    public Slider volumeSlider;
+    Text volumeLabel;
+    public void Start()
+    {
+        volumeLabel = GameObject.Find("VolumeText").GetComponent<Text>();
+        unPauseGame();
+
+    }
+
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        pausePanel.SetActive(true);
+    }
+    public void unPauseGame()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        pausePanel.SetActive(false);
+    }
+
+    public void OnValueChanged()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    public void volumeLabelUpdate(float volumeValue)
+    {
+        float valueToBeUpdated = Mathf.RoundToInt(volumeValue * 100);
+        volumeLabel.text = valueToBeUpdated + "%";
+    }
+
+
+}
