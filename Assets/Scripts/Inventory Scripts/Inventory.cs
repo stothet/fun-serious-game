871fb26be443019f4a7c91737c8b8789ba72d6ed
jxturtle <jxia616@aucklanduni.ac.nz>
2017-10-23 18:22:22 +1300

    Removed placeholder item

--- Assets/Scripts/Inventory Scripts/Inventory.cs
+++ Assets/Scripts/Inventory Scripts/Inventory.cs
@@ -58,8 +58,8 @@ public class Inventory : MonoBehaviour {
             addItem(PersistenceController.instance.inventoryState.database[i]);
         }
         // Add items you want to show up in the inventory here. The number is the unique ID of the item.
-		addItem(Configuration.bruceRegisterEntryName);
 
+		//addItem(Configuration.bruceRegisterEntryName);
         //addItem(1);
 		panel.SetActive(false);
 	}
