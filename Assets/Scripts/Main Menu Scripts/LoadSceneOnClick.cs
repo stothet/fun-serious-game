e2d4eb24b4fb6cf63a65182149bdf76f8c57c622
xavier630 <xavier630@gmail.com>
2017-10-23 17:01:39 +1300

    Make saving and loading work with scene transitions

--- Assets/Scripts/Main Menu Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/Main Menu Scripts/LoadSceneOnClick.cs
@@ -13,4 +13,32 @@ public class LoadSceneOnClick : MonoBehaviour
     {
         SceneManager.LoadScene(sceneIndex);
     }
+    public void LoadByState()
+    {
+        Debug.Log("loading scene number " + PersistenceController.currentScene);
+        SceneManager.LoadScene(PersistenceController.currentScene);
+    }
+
+    public void setCurrentSceneStateByIndex(string name)
+    {
+        PersistenceController.currentScene = name;
+    }
+
+    public void loadAct1()
+    {
+        string act = Configuration.worldMapSceneName;
+        setCurrentSceneStateByIndex(act);
+        SceneManager.LoadScene(act);
+    }
+    public void loadAct2()
+    {
+        string act = Configuration.act2SceneSceneName;
+        setCurrentSceneStateByIndex(act);
+        SceneManager.LoadScene(act);
+    }
+    public void loadMenu()
+    {
+        string act = Configuration.mainMenuSceneName;
+        SceneManager.LoadScene(act); // Don't save menu as last scene
+    }
 }
