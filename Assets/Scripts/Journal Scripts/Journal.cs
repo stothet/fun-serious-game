6fbbf105242b16abddf04e944adaad9b217e596f
Shibani T <shibani.timblo@gmail.com>
2017-10-23 16:54:20 +1300

    journal entries now have action listeners

--- Assets/Scripts/Journal Scripts/Journal.cs
+++ Assets/Scripts/Journal Scripts/Journal.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 /// <summary>
@@ -8,12 +10,16 @@ using UnityEngine.UI;
 public class Journal : MonoBehaviour {
     public Text journalEntry;
     public GameObject JournalSlot;
+    private Text selectedText;
+    public List<Button> _entries = new List<Button>();
+    
     public void Start()
     {
         for(int i = 0; i<PersistenceController.instance.journalState.journal.Count; i++)
         {
             putJournalEntry(PersistenceController.instance.journalState.journal[i]);
         }
+        
     }
     
     /// <summary>
@@ -26,6 +32,10 @@ public class Journal : MonoBehaviour {
         GameObject slot = GameObject.Instantiate(JournalSlot);
         slot.AddComponent<RectTransform>();
         slot.AddComponent<Button>();
+        Button b = slot.GetComponentInChildren<Button>();
+        b.onClick.AddListener(TaskOnClick);
+        _entries.Add(b);
+        Debug.Log("HERe1");
         journalEntry = slot.GetComponentInChildren<Text>();
         slot.transform.SetParent(this.gameObject.transform, false);
         journalEntry.text = txt;
@@ -35,4 +45,23 @@ public class Journal : MonoBehaviour {
         }
         //journalEntry.text = PersistenceController.JournalState.journal;
     }
+
+    void TaskOnClick()
+    {
+        Debug.Log("HERe2");
+        GameObject selectedSlot = EventSystem.current.currentSelectedGameObject;
+        selectedText = selectedSlot.GetComponentInChildren<Text>();
+
+    }
+
+    /// <summary>
+    /// Returns the currently selected item by the player.
+    /// </summary>
+    /// <returns></returns>
+    public Text GetSelectedEntry()
+    {
+        return selectedText;
+    }
+    
+    
 }
